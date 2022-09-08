3ec9b1986fd05e9d53db5cb9ceacc036b694dd50
Fabricio Quintana <88584654+quintanafabri@users.noreply.github.com>
2022-09-08 16:12:12 -0300

    FINAL

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 
 
 let fotoid
-marcelo = true
 
-function marce(){
-    if (marcelo === true ){
-        document.getElementById("marce").innerHTML = "MARCE DIVINO"
-    }
-}
+
+
 
 
 function numeroRandomMath() {  // Funcion para generar el numero random .
     let numero = Math.round( Math.random() * (21 - 0) + 0);
     fotoid = numero;
     ImagenLoad();
-    marce();
    
 }
 
@@ -22,7 +17,7 @@ function ImagenLoad(){ // Funcion que generara la imagen
     let imagenHTML = '';
     let fotoidStr = (fotoid+1).toString(); // pasamos fotoid a STRING
 
-    imagenHTML = `    <img id="imagen" src="https://github.com/quintanafabri/AmigosAPP/blob/main/MarceFotos/Marce_`+fotoidStr+`.jpg" alt="">
+    imagenHTML = `    <img id="imagen" src="https://quintanafabri.github.io/AmigosAPP/`+amigos.value+`Fotos/`+amigos.value+`_`+fotoidStr+`.jpg" alt="">
     `
 
 document.getElementById("foto").innerHTML = imagenHTML;
